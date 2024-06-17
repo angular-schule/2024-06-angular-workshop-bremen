166b88942965b6e9b291c927de827f1ed041ebe2
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-06-17 10:21:14 +0200

    kleine einstellungen zum Anfang

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+
+    // provideZoneChangeDetection({ eventCoalescing: true }),
+    // das hier bitte nicht auf Prod einsetzen! (nerds freuen sich)
+    provideExperimentalZonelessChangeDetection(),
+
+    provideRouter(routes)]
 };
