5301230b3b7f80d8823172a4cf06e24b991b77d9
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-06-18 15:56:22 +0200

    appComonent gefixt

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -1,29 +1,48 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { DashboardComponent } from './books/dashboard/dashboard.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  template: 'Hallo Bremen! 🧡'
+})
+export class DummyDashboardComponent {}
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-    }).compileComponents();
+    })
+    // Shallow Component Test
+    // .overrideComponent(AppComponent, {
+    //   set: { imports: [], schemas: [NO_ERRORS_SCHEMA] }
+    // })
+    .overrideComponent(AppComponent, {
+      remove: { imports: [DashboardComponent] },
+      add: { imports: [DummyDashboardComponent] }
+    })
+    .compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  // it('should create the app', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app).toBeTruthy();
+  // });
 
-  it(`should have the 'book-rating' title`, () => {
+  it(`should have the 'Book Rating' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('book-rating');
+    expect(app.title).toEqual('Book Rating');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, book-rating');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Book Rating');
   });
 });
