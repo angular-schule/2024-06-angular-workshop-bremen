75462d04bb8f33a850f6aad85489ffb7ea7e5f12
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-06-18 14:14:11 +0200

    testing: hands on!

---
+++ book-rating/src/app/books/shared/book-rating.service.spec.ts
@@ -0,0 +1,51 @@
+import { Book } from './book';
+import { BookRatingService } from './book-rating.service';
+
+fdescribe('BookRatingService', () => {
+  let service: BookRatingService;
+  let book: Book;
+
+  // Arrange
+  beforeEach(() => {
+    service = new BookRatingService();
+    book = {
+      isbn: '000',
+      title: 'Test',
+      description: 'Test',
+      rating: 3
+    };
+  });
+
+  // JIRA-5789 - Books as on Amazon
+  it('should rate up a book by one', () => {
+
+    // Act
+    const ratedBook = service.rateUp(book);
+
+    // Assert
+    expect(ratedBook.rating).toBe(4);
+  });
+
+  it('should rate down a book by one', () => {
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(2);
+  });
+
+  it('should not be allowed to have a rating greater than 5', () => {
+    book.rating = 5;
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(5);
+  });
+
+  it('should not be allowed to have a rating smaller than 1', () => {
+    book.rating = 1;
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(1);
+  });
+
+  it('should not mutate the book', () => {
+    const frozenBook = Object.freeze(book);
+    expect(() => service.rateUp(frozenBook)).not.toThrow();
+    expect(() => service.rateDown(frozenBook)).not.toThrow();
+  })
+});
