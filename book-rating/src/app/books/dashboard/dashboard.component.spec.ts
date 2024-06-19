f558e3d12a3037fee298acfc5206329305e55803
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-06-19 11:10:21 +0200

    testing behaviour with spies

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
+
+    const bookRatingMock = {
+      rateUp: (book: Book) => book
+      // Absichtlich keine rateDown (halbes Entchen)
+    }
+
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
+      imports: [DashboardComponent], // immer noch ein Integration-Test, weil BookComponent
+      providers: [{
+        provide: BookRatingService,
+        useValue: bookRatingMock
+      }]
     })
     .compileComponents();
 
@@ -17,7 +29,14 @@ describe('DashboardComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('doRateUp() should call the BookRatingService', () => {
+
+    const br = TestBed.inject(BookRatingService);
+    spyOn(br, 'rateUp').and.callThrough();
+
+    const book = {} as Book;
+    component.doRateUp(book);
+
+    expect(br.rateUp).toHaveBeenCalledOnceWith(book);
   });
 });
--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Book } from '../shared/book';
 import { JsonPipe, UpperCasePipe } from '@angular/common';
 import { BookComponent } from '../book/book.component';
+import { BookRatingService } from '../shared/book-rating.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,12 +13,17 @@ import { BookComponent } from '../book/book.component';
 })
 export class DashboardComponent {
 
+  // Alter Stil: constructor(private bookRatingService: BookRatingService) {}
+
+  // Neuer Stil mit inject() --> siehe Artikel
+  bookRatingService = inject(BookRatingService);
+
   // 🦆
-  books: Book[] = [{
+  books = signal<Book[]>([{
     isbn: '000',
     title: 'jQuery',
     description: 'Altes Buch',
-    rating: 1
+    rating: 5
   }, {
     isbn: '111',
     title: 'AngularJS',
@@ -27,14 +33,36 @@ export class DashboardComponent {
     isbn: '222',
     title: 'Angular',
     description: 'Beste Buch der Welt',
-    rating: 5
-  }];
+    rating: 1
+  }]);
 
   doRateUp(book: Book) {
-    console.log(book);
+    const ratedBook = this.bookRatingService.rateUp(book);
+
+    // const ratedBook = {
+    //   ...book,
+    //   rating: Math.min(book.rating + 1, 5)
+    // }
+
+    this.updateAndSort(ratedBook);
   }
 
   doRateDown(book: Book) {
-    console.table(book);
+    const ratedBook = this.bookRatingService.rateDown(book);
+    this.updateAndSort(ratedBook);
+  }
+
+  updateAndSort(ratedBook: Book) {
+    const updatedBooks = this.books()
+      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+      .sort((a,b) => b.rating - a.rating);
+
+    this.books.set(updatedBooks);
+
+    // ODER Update
+
+    // this.books.update(books => books
+    //   .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+    //   .sort((a,b) => b.rating - a.rating));
   }
 }
