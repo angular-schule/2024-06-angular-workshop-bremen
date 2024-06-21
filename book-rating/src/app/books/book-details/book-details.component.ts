290513774188179a3ce6c7b64ec24e308fc96e2f
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-06-21 14:25:20 +0200

    rxjs - merge map, reihenfolge wird nicht "korrigiert"

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,15 +1,27 @@
-import { Component, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, input } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { map, mergeMap } from 'rxjs';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, JsonPipe],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
 export class BookDetailsComponent {
 
-  isbn = input<string>();
+  // isbn = input<string>();
+
+  router = inject(ActivatedRoute);
+  bookStore = inject(BookStoreService);
+
+  book$ = toSignal(this.router.paramMap.pipe(
+    map(paramMap => paramMap.get('isbn') || ''),
+    mergeMap(isbn => this.bookStore.getSingleBook(isbn))
+  ));
 
 }
