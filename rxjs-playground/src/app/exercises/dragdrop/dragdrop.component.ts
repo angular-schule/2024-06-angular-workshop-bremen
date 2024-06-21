040426ad1bf6144d7644148344cba69b38afe48e
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-06-21 16:14:02 +0200

    TODO: fix me! :-)

--- rxjs-playground/src/app/exercises/dragdrop/dragdrop.component.ts
+++ rxjs-playground/src/app/exercises/dragdrop/dragdrop.component.ts
@@ -1,47 +1,29 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { fromEvent, concatMap, takeUntil } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { fromEvent, concatMap, takeUntil, map } from 'rxjs';
 
 @Component({
   templateUrl: './dragdrop.component.html',
   styleUrls: ['./dragdrop.component.scss'],
   standalone: true
 })
-export class DragdropComponent implements OnInit {
+export class DragdropComponent {
 
   @ViewChild('target', { static: true }) target!: ElementRef<HTMLElement>;
-  targetPosition = [100, 80];
 
-  ngOnInit() {
-    const mouseMove$ = fromEvent<MouseEvent>(document, 'mousemove');
-    const mouseDown$ = fromEvent<MouseEvent>(this.target.nativeElement, 'mousedown');
-    const mouseUp$ = fromEvent<MouseEvent>(document, 'mouseup');
+  mouseMove$ = fromEvent<MouseEvent>(document, 'mousemove');
+  // mouseDown$ = fromEvent<MouseEvent>(this.target.nativeElement, 'mousedown');
+  mouseDown$ = fromEvent<MouseEvent>(document, 'mousedown');
+  mouseUp$ = fromEvent<MouseEvent>(document, 'mouseup');
 
-    /**
-     * Nutze RxJS, um die rote Box mit Drag-and-drop zu bewegen.
-     *
-     * Die Methode setTargetPosition(e: MouseEvent) ändert die Position der Box.
-     * Nutze die Observables mouseMove$, mouseDown$ und mouseUp$ in einer geeigneten Kombination.
-     * Beginne damit, dass die Box am Mauszeiger klebt.
-     * Sorge dann dafür, dass dieser Prozess erst beim Klick (mouseDown$) beginnt.
-     * Beende den Prozess, sobald mouseUp$ feuert.
-     */
-
-    /******************************/
-
-    mouseDown$.pipe(
-      concatMap(() => mouseMove$.pipe(takeUntil(mouseUp$)))
-    ).subscribe(e => this.setTargetPosition(e))
-
-
-    /******************************/
-  }
-
-  private setTargetPosition(event: MouseEvent) {
-    const offset = 50;
-    this.targetPosition = [
-      event.pageX - offset,
-      event.pageY - offset
-    ];
-  }
-
+  targetPosition = toSignal(this.mouseDown$.pipe(
+    concatMap(() => this.mouseMove$.pipe(takeUntil(this.mouseUp$))),
+    map(event => {
+      const offset = 50;
+      return {
+        x: event.pageX - offset,
+        y: event.pageY - offset
+      }
+    })
+  ), { initialValue: { x: 100, y: 80 } });
 }
